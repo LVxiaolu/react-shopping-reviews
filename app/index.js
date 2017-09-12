f17e1886bfc4e5bfffdf82875da2de980ff9c8c0
LVxiaolu <LVxiaolu8023@163.com>
2017-09-12 17:12:34 +0800

    redux

--- app/index.js
+++ app/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
-import ReactDOM,{render} from 'react-dom';
+import ReactDOM, {render} from 'react-dom';
 import './assets/index.less'
 import App from "./contaniers/index";
+import {Provider} from 'react-redux'
+//导入store
+import {configureStore} from './store';
+//可以在configureStore传入初始状态
+let store = configureStore();//生成store
 
-render(<App/>,document.querySelector('#root'));
+render(
+  <Provider store={store}>
+    <App/>
+  </Provider>
+  , document.querySelector('#root'));
 
