9b49f32c66a9c9192c9102432cde0b71c2365552
LVxiaolu <LVxiaolu8023@163.com>
2017-09-12 15:01:03 +0800

    Ad

---
+++ mock/home/ad.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        title: '兄弟盱眙龙虾',
+        img: 'http://p0.meituan.net/deal/d08dd70492fe8602e224ea67dc90acb6109436.jpg%40120w_90h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+        link: 'http://m.dianping.com/tuan/deal/24465351?from=m_discount'
+    },
+    {
+        title: '大团鸭头',
+        img: 'http://p0.meituan.net/deal/65468682f387951e9aa175f125b32afc58415.jpg%40120w_90h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+        link: '//m.dianping.com/tuan/deal/17231425?from=m_discount.cn'
+    },
+    {
+        title: 'PE Salon',
+        img: 'http://p1.meituan.net/dpdeal/e49c9ab8af35a2be2cbb0557f1d6606a54274.jpg%40120w_90h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+        link: 'http://m.dianping.com/tuan/deal/14243051?from=m_discount'
+    },
+    {
+        title: '薇琳医疗美容医院',
+        img: 'http://p0.meituan.net/dpdeal/773ad67b01ecddee1249086dc71634f2184053.jpg%40120w_90h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+        link: 'http://m.dianping.com/tuan/deal/26543234?from=m_discount'
+    },
+    {
+        title: '明翼舞蹈会所',
+        img: 'http://p0.meituan.net/dpdeal/a883b19926b91870bc1e869373164feb76179.jpg%40120w_90h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+        link: 'http://m.dianping.com/tuan/deal/6213456?from=m_discount'
+    },
+    {
+        title: '金樱汗蒸洗浴',
+        img: '//p1.meituan.net/dpdeal/8b6a3ece417f9fb499148922f7951cb275610.jpg%40120w_90h_1e_1c_1l%7Cwatermark%3D1%26%26r%3D1%26p%3D9%26x%3D20%26y%3D20',
+        link: 'http://m.dianping.com/tuan/deal/6400326?from=m_discount'
+    }
+]
