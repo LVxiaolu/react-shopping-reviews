e0d1ee694d1e8c9972034ab2e18364be94e65b9f
LVxiaolu <LVxiaolu8023@163.com>
2017-09-13 20:49:32 +0800

    OrderList 提交评价

--- mock/server.js
+++ mock/server.js
@@ -31,3 +31,14 @@ app.get('/api/detail/comment/:id/:page',(req,res)=>{
   console.log(req.params.page);
   res.send(comment);
 });
+
+//订单列表 用户名  /api/orderlist/:username
+let orderList = require('./orderlist/orderList');
+app.get('/api/orderlist/:username',((req,res)=>{
+  res.send(orderList);
+}));
+//提交评论  body-parser  req.body
+app.post('/api/comment',(req,res)=>{
+  console.log(req.body);
+  res.send({msg:'ok'});
+});
